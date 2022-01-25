8a48d31f123def1885adf0764e258c6437506b62
park-ing <yh.park8742@gmail.com>
2022-01-25 22:18:54 +0900

    v4

--- ch4.py
+++ ch4.py
@@ -227,6 +227,7 @@ class TwoLayerNet:
         # 가중치 초기화
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        # np.random.randn(a, b) -> (a,b)크기 행렬로 정규분포로 초기화. 랜덤한 숫자로 생성
         self.params['b1'] = np.zeros(hidden_size) # 이 예시에는 0으로 초기화
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
