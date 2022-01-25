3a2909b565d474c16fac0ae6612c99f7115b9f93
park-ing <yh.park8742@gmail.com>
2022-01-26 02:50:27 +0900

    v2

--- ch5.py
+++ ch5.py
@@ -1,1 +1,90 @@
 #chapter5 오차역전파법
+
+######################
+# 단순한 역전파 계층 구현하기
+# 역전파 곱셈 계층
+class MulLayer:
+    def __init__(self): # 순전파 시의 입력 값을 유지하기 위해서 초기화 사용.
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * self.y # x와 y를 바꾼다.
+        dy = dout * self.x
+
+        return dx, dy
+
+apple = 100
+apple_num = 2
+tax = 1.1
+
+# 계층들
+mul_apple_layer = MulLayer()
+mul_tax_layer = MulLayer()
+
+# 순전파
+apple_price = mul_apple_layer.forward(apple, apple_num)
+price = mul_tax_layer.forward(apple_price, tax)
+
+print(price) # 220
+
+# 역전파
+dprice = 1
+dapple_price, dtax = mul_tax_layer.backward(dprice)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+print(dapple, dapple_num, dtax) # 2.2 110 200
+
+######덧셈 계층
+class AddLayer:
+    def __init__(self):
+        pass            # pass -> 이 함수에서는 아무것도 하지 말라.
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+# layer
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+
+# forward
+apple_price = mul_apple_layer.forward(apple, apple_num)  # (1)
+orange_price = mul_orange_layer.forward(orange, orange_num)  # (2)
+all_price = add_apple_orange_layer.forward(apple_price, orange_price)  # (3)
+price = mul_tax_layer.forward(all_price, tax)  # (4)
+
+# backward
+dprice = 1
+dall_price, dtax = mul_tax_layer.backward(dprice)  # (4)
+dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)  # (3)
+dorange, dorange_num = mul_orange_layer.backward(dorange_price)  # (2)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)  # (1)
+
+print("price:", int(price))
+print("dApple:", dapple)
+print("dApple_num:", int(dapple_num))
+print("dOrange:", dorange)
+print("dOrange_num:", int(dorange_num))
+print("dTax:", dtax)
+
