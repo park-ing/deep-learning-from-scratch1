fd196a55d69e8808feedd95234a7619a93a9c064
park-ing <yh.park8742@gmail.com>
2022-01-25 22:11:46 +0900

    v3

--- ch4.py
+++ ch4.py
@@ -33,7 +33,7 @@ print(cross_entropy_error(np.array(y), np.array(t))) # 0.510825457099338
 #######################################
 # 미니배치 학습
 from dataset.mnist import load_mnist
-
+'''
 (x_train, t_train), (x_test, t_test) = \
     load_mnist(normalize = True, one_hot_label=True)
 
@@ -54,7 +54,7 @@ print()
 print(t_batch)
 
 print(np.random.choice(60000,10)) # [0,60000) 범위 안에서 랜덤한 인덱스 10개 출력!
-
+'''
 
 #######배치용 교차 엔트로피 오차 계산하기
 def cross_entropy_error(y, t):
@@ -218,3 +218,111 @@ print(dw)
  '''
 ####################################
 # 학습 알고리즘 구현하기
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # 가중치 초기화
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size) # 이 예시에는 0으로 초기화
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+
+    # x : 입력 데이터, t : 정답 레이블
+    def loss(self, x, t):
+        y = self.predict(x)
+
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0]) 
+        return accuracy
+
+    # x : 입력 데이터, t : 정답 레이블
+    def numerical_gradient_mul(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient_mul(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient_mul(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient_mul(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient_mul(loss_W, self.params['b2'])
+
+        return grads
+
+# mnist 데이터셋을 사용하여 학습 수행
+####****** 지금은 수치미분방식이라 시간이 엄청 오래 걸린다.
+from dataset.mnist import load_mnist
+import matplotlib.pyplot as plt
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize = True, one_hot_label=True)
+
+
+# 하이퍼파라미터
+iters_num = 10000  # 반복 횟수
+train_size = x_train.shape[0]
+batch_size = 100   # 미니배치 크기
+learning_rate = 0.1
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+# 1에폭당 반복 수
+iter_per_epoch = max(train_size / batch_size, 1)
+
+for i in range(iters_num):
+    #미니배치 휙득
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+    
+    # 기울기 계산
+    grad = network.numerical_gradient_mul(x_batch, t_batch)
+    
+    # 매개변수 갱신
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+    
+    # 학습 경과 기록
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+	# 1에폭당 정확도 계산
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | "+ str(train_acc) + ", " + str(test_acc))
+
+# 그래프 그리기
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train acc')
+plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
