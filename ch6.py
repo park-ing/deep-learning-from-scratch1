05dedae500aa4541435be1fadef8365e6a016f05
park-ing <yh.park8742@gmail.com>
2022-01-26 17:22:19 +0900

    v2

--- ch6.py
+++ ch6.py
@@ -1,4 +1,7 @@
 ##### chapter6 학습 관련 기술들
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import OrderedDict
 
 # SGD 복습
 class SGD:
@@ -8,3 +11,217 @@ class SGD:
     def update(self, params, grads):
         for key in params.keys():
             params[key] -= self.lr * grads[key]
+
+'''
+# 신경망에서 SGD클래스 사용하기 위한 의사코드
+network = TwoLayerNet(...)
+optimizer = SGD() # 클래스를 분리해 기능을 모듈화하기 좋다.
+
+for i in range(10000):
+    ...
+    x_batch, t_batch = get_mini_batch(...) # 미니배치
+    grads = network.gradient(x_batch, t_batch)
+    params = network.params
+    optimizer.update(params, grads)
+    ...
+'''
+
+##### 모멘텀
+class Momentum:
+
+    """모멘텀 SGD"""
+
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+        
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():                                
+                self.v[key] = np.zeros_like(val)
+                
+        for key in params.keys():
+            self.v[key] = self.momentum*self.v[key] - self.lr*grads[key] 
+            params[key] += self.v[key]
+
+#####AdaGrad(apaptive gradient)
+class AdaGrad:
+
+    """AdaGrad"""
+
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        self.h = None
+        
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+            
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+	 # 1e - 7이라는 작은 값을 더해서 self.h[key]에 0이 담겨 있다 해도 0으로 나누는 사태를 막아준다.
+
+#####Adam
+class Adam:
+
+    """Adam (http://arxiv.org/abs/1412.6980v8)"""
+
+    def __init__(self, lr=0.001, beta1=0.9, beta2=0.999):
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.iter = 0
+        self.m = None
+        self.v = None
+        
+    def update(self, params, grads):
+        if self.m is None:
+            self.m, self.v = {}, {}
+            for key, val in params.items():
+                self.m[key] = np.zeros_like(val)
+                self.v[key] = np.zeros_like(val)
+        
+        self.iter += 1
+        lr_t  = self.lr * np.sqrt(1.0 - self.beta2**self.iter) / (1.0 - self.beta1**self.iter)         
+        
+        for key in params.keys():
+            #self.m[key] = self.beta1*self.m[key] + (1-self.beta1)*grads[key]
+            #self.v[key] = self.beta2*self.v[key] + (1-self.beta2)*(grads[key]**2)
+            self.m[key] += (1 - self.beta1) * (grads[key] - self.m[key])
+            self.v[key] += (1 - self.beta2) * (grads[key]**2 - self.v[key])
+            
+            params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + 1e-7)
+            
+            #unbias_m += (1 - self.beta1) * (grads[key] - self.m[key]) # correct bias
+            #unbisa_b += (1 - self.beta2) * (grads[key]*grads[key] - self.v[key]) # correct bias
+            #params[key] += self.lr * unbias_m / (np.sqrt(unbisa_b) + 1e-7)
+
+
+######갱신방법 예시(시각화)
+def f(x, y):
+    return x**2 / 20.0 + y**2
+
+
+def df(x, y):
+    return x / 10.0, 2.0*y
+
+init_pos = (-7.0, 2.0)
+params = {}
+params['x'], params['y'] = init_pos[0], init_pos[1]
+grads = {}
+grads['x'], grads['y'] = 0, 0
+
+
+optimizers = OrderedDict()
+optimizers["SGD"] = SGD(lr=0.95)
+optimizers["Momentum"] = Momentum(lr=0.1)
+optimizers["AdaGrad"] = AdaGrad(lr=1.5)
+optimizers["Adam"] = Adam(lr=0.3)
+
+idx = 1
+
+for key in optimizers:
+    optimizer = optimizers[key]
+    x_history = []
+    y_history = []
+    params['x'], params['y'] = init_pos[0], init_pos[1]
+    
+    for i in range(30):
+        x_history.append(params['x'])
+        y_history.append(params['y'])
+        
+        grads['x'], grads['y'] = df(params['x'], params['y'])
+        optimizer.update(params, grads)
+    
+
+    x = np.arange(-10, 10, 0.01)
+    y = np.arange(-5, 5, 0.01)
+    
+    X, Y = np.meshgrid(x, y) 
+    Z = f(X, Y)
+    
+    # 외곽선 단순화
+    mask = Z > 7
+    Z[mask] = 0
+    
+    # 그래프 그리기
+    plt.subplot(2, 2, idx)
+    idx += 1
+    plt.plot(x_history, y_history, 'o-', color="red")
+    plt.contour(X, Y, Z)
+    plt.ylim(-10, 10)
+    plt.xlim(-10, 10)
+    plt.plot(0, 0, '+')
+    #colorbar()
+    #spring()
+    plt.title(key)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    
+plt.show()
+
+
+
+'''
+######MNIST 데이터셋으로 갱신 방법 비교
+# 0. MNIST 데이터 읽기==========
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+
+train_size = x_train.shape[0]
+batch_size = 128
+max_iterations = 2000
+
+
+# 1. 실험용 설정==========
+optimizers = {}
+optimizers['SGD'] = SGD()
+optimizers['Momentum'] = Momentum()
+optimizers['AdaGrad'] = AdaGrad()
+optimizers['Adam'] = Adam()
+#optimizers['RMSprop'] = RMSprop()
+
+networks = {}
+train_loss = {}
+for key in optimizers.keys():
+    networks[key] = MultiLayerNet(
+        input_size=784, hidden_size_list=[100, 100, 100, 100],
+        output_size=10)
+    train_loss[key] = []    
+
+
+# 2. 훈련 시작==========
+for i in range(max_iterations):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+    
+    for key in optimizers.keys():
+        grads = networks[key].gradient(x_batch, t_batch)
+        optimizers[key].update(networks[key].params, grads)
+    
+        loss = networks[key].loss(x_batch, t_batch)
+        train_loss[key].append(loss)
+    
+    if i % 100 == 0:
+        print( "===========" + "iteration:" + str(i) + "===========")
+        for key in optimizers.keys():
+            loss = networks[key].loss(x_batch, t_batch)
+            print(key + ":" + str(loss))
+
+
+# 3. 그래프 그리기==========
+markers = {"SGD": "o", "Momentum": "x", "AdaGrad": "s", "Adam": "D"}
+x = np.arange(max_iterations)
+for key in optimizers.keys():
+    plt.plot(x, smooth_curve(train_loss[key]), marker=markers[key], markevery=100, label=key)
+plt.xlabel("iterations")
+plt.ylabel("loss")
+plt.ylim(0, 1)
+plt.legend()
+plt.show()
+'''
