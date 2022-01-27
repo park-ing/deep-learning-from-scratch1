d346e5358a2e0928a650536cc47f0349d5da5090
park-ing <yh.park8742@gmail.com>
2022-01-27 15:42:09 +0900

    v5

--- ch6.py
+++ ch6.py
@@ -338,3 +338,147 @@ plt.ylabel("loss")
 plt.ylim(0, 2.5)
 plt.legend()
 plt.show()
+
+from common.multi_layer_net_extend import MultiLayerNetExtend
+##############################################
+# 배치 정규화를 MNIST로 테스트(학습 속도 향상)
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+
+# 학습 데이터를 줄임
+x_train = x_train[:1000]
+t_train = t_train[:1000]
+
+max_epochs = 20
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.01
+
+
+def __train(weight_init_std):
+    bn_network = MultiLayerNetExtend(input_size=784, hidden_size_list=[100, 100, 100, 100, 100], output_size=10, 
+                                    weight_init_std=weight_init_std, use_batchnorm=True)
+    network = MultiLayerNetExtend(input_size=784, hidden_size_list=[100, 100, 100, 100, 100], output_size=10,
+                                weight_init_std=weight_init_std)
+    optimizer = SGD(lr=learning_rate)
+    
+    train_acc_list = []
+    bn_train_acc_list = []
+    
+    iter_per_epoch = max(train_size / batch_size, 1)
+    epoch_cnt = 0
+    
+    for i in range(1000000000):
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+    
+        for _network in (bn_network, network):
+            grads = _network.gradient(x_batch, t_batch)
+            optimizer.update(_network.params, grads)
+    
+        if i % iter_per_epoch == 0:
+            train_acc = network.accuracy(x_train, t_train)
+            bn_train_acc = bn_network.accuracy(x_train, t_train)
+            train_acc_list.append(train_acc)
+            bn_train_acc_list.append(bn_train_acc)
+    
+            print("epoch:" + str(epoch_cnt) + " | " + str(train_acc) + " - " + str(bn_train_acc))
+    
+            epoch_cnt += 1
+            if epoch_cnt >= max_epochs:
+                break
+                
+    return train_acc_list, bn_train_acc_list
+
+
+# 그래프 그리기==========
+weight_scale_list = np.logspace(0, -4, num=16)
+x = np.arange(max_epochs)
+
+for i, w in enumerate(weight_scale_list):
+    print( "============== " + str(i+1) + "/16" + " ==============")
+    train_acc_list, bn_train_acc_list = __train(w)
+    
+    plt.subplot(4,4,i+1)
+    plt.title("W:" + str(w))
+    if i == 15:
+        plt.plot(x, bn_train_acc_list, label='Batch Normalization', markevery=2)
+        plt.plot(x, train_acc_list, linestyle = "--", label='Normal(without BatchNorm)', markevery=2)
+    else:
+        plt.plot(x, bn_train_acc_list, markevery=2)
+        plt.plot(x, train_acc_list, linestyle="--", markevery=2)
+
+    plt.ylim(0, 1.0)
+    if i % 4:
+        plt.yticks([])
+    else:
+        plt.ylabel("accuracy")
+    if i < 12:
+        plt.xticks([])
+    else:
+        plt.xlabel("epochs")
+    plt.legend(loc='lower right')
+    
+plt.show()
+
+##############################################
+# 오버피팅 환경을 만들어 테스트
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+
+# 오버피팅을 재현하기 위해 학습 데이터 수를 줄임
+x_train = x_train[:300]
+t_train = t_train[:300]
+
+# weight decay（가중치 감쇠） 설정 =======================
+#weight_decay_lambda = 0 # weight decay를 사용하지 않을 경우
+weight_decay_lambda = 0.1
+# ====================================================
+
+network = MultiLayerNet(input_size=784, hidden_size_list=[100, 100, 100, 100, 100, 100], output_size=10,
+                        weight_decay_lambda=weight_decay_lambda)
+optimizer = SGD(lr=0.01) # 학습률이 0.01인 SGD로 매개변수 갱신
+
+max_epochs = 201
+train_size = x_train.shape[0]
+batch_size = 100
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
+epoch_cnt = 0
+
+for i in range(1000000000):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    grads = network.gradient(x_batch, t_batch)
+    optimizer.update(network.params, grads)
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+
+        print("epoch:" + str(epoch_cnt) + ", train acc:" + str(train_acc) + ", test acc:" + str(test_acc))
+
+        epoch_cnt += 1
+        if epoch_cnt >= max_epochs:
+            break
+
+
+# 그래프 그리기==========
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(max_epochs)
+plt.plot(x, train_acc_list, marker='o', label='train', markevery=10)
+plt.plot(x, test_acc_list, marker='s', label='test', markevery=10)
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
+
