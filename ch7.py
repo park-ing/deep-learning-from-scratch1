851b26755f14a63b85bf2dce108ccd8c0e1cd2ae
park-ing <yh.park8742@gmail.com>
2022-01-29 01:59:57 +0900

    v4

--- ch7.py
+++ ch7.py
@@ -1,5 +1,7 @@
 # chapter7 합성곱 신경망(CNN)
+from typing import OrderedDict
 import numpy as np
+from collections import OrderedDict
 ################
 # 4차원 배열
 x = np.random.rand(10,1,28,28) # 무작위로 데이터 생성
@@ -12,7 +14,7 @@ print(x.shape)
 
 ############################
 # 합성곱 계층 구현하기
-from common.util import im2col
+from common.util import conv_output_size, im2col
 ## im2col(input_data, filter_h, filter_w, stride=1,pad=0)
 #   input_data : (데이터 수, 채널 수, 높이 너비)의 4차원 배열로 이뤄진 입력 데이터
 #   filter_h : 필터의 높이
@@ -80,3 +82,199 @@ class Pooling:
         return out
 
 
+####################################
+# CNN 구현하기
+
+# 초기화 떄 받는 인수
+# input_dim : 입력 데이터(채널 수, 높이, 너비)의 차원
+# conv_param : 합성곱 계층의 하이퍼파라미터(딕셔너리), 딕셔너리의 키는 다음과 같다.
+#   - filter_num : 필터 수
+#   - filter_size : 필터 크기
+#   - stride : 스트라이드
+#   - pad : 패딩
+#   - hidden_size : 은닉층(완전연결)의 뉴런 수
+#   - output_size : 출력층(완전연결)의 뉴런 수
+#   - weight_init_std : 초기화 때의 가중치 표준편차
+from common.layers import *
+from common.gradient import numerical_gradient
+import pickle
+
+class SimpleConvNet:
+    def __init__(self, input_dim=(1,28,28), conv_param = {'filter_num':30, 'filter_size':5, 'pad':0,'stride':1},hidden_size=100,output_size=10, weight_init_std=0.01):
+        filter_num = conv_param['filter_num']
+        filter_size = conv_param['filter_size']
+        filter_pad = conv_param['pad']
+        filter_stride = conv_param['stride']
+        input_size = input_dim[1]
+        conv_output_size = (input_size - filter_size + 2*filter_pad) / filter_stride + 1
+        pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
+
+        # 가중치 매개변수 초기화
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+        self.params['b1'] = np.zeros(filter_num)
+        self.params['W2'] = weight_init_std * np.random.randn(pool_output_size, hidden_size)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W3'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b3'] = np.zeros(output_size)
+
+        # CNN을 구성하는 계층 생성
+        self.layers = OrderedDict()
+        self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'],
+                                           conv_param['stride'], conv_param['pad'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
+        self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['Relu2'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
+
+        self.last_layer = SoftmaxWithLoss()
+
+    # 추론을 수행
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+
+        return x
+
+    # 손실 함수의 값을 구한다.
+    def loss(self, x, t):
+        """손실 함수를 구한다.
+        Parameters
+        ----------
+        x : 입력 데이터
+        t : 정답 레이블
+        """
+        y = self.predict(x)
+        return self.last_layer.forward(y, t)
+
+    # 오차역전파법으로 기울기를 구한다.
+    def gradient(self, x, t):
+        """기울기를 구한다(오차역전파법).
+
+        Parameters
+        ----------
+        x : 입력 데이터
+        t : 정답 레이블
+
+        Returns
+        -------
+        각 층의 기울기를 담은 사전(dictionary) 변수
+            grads['W1']、grads['W2']、... 각 층의 가중치
+            grads['b1']、grads['b2']、... 각 층의 편향
+        """
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.last_layer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 결과 저장
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Conv1'].dW, self.layers['Conv1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W3'], grads['b3'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+
+        return grads
+
+    def accuracy(self, x, t, batch_size=100):
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
+        
+        acc = 0.0
+        
+        for i in range(int(x.shape[0] / batch_size)):
+            tx = x[i*batch_size:(i+1)*batch_size]
+            tt = t[i*batch_size:(i+1)*batch_size]
+            y = self.predict(tx)
+            y = np.argmax(y, axis=1)
+            acc += np.sum(y == tt) 
+        
+        return acc / x.shape[0]    
+    
+    def save_params(self, file_name="params.pkl"):
+        params = {}
+        for key, val in self.params.items():
+            params[key] = val
+        with open(file_name, 'wb') as f:
+            pickle.dump(params, f)
+
+    def load_params(self, file_name="params.pkl"):
+        with open(file_name, 'rb') as f:
+            params = pickle.load(f)
+        for key, val in params.items():
+            self.params[key] = val
+
+        for i, key in enumerate(['Conv1', 'Affine1', 'Affine2']):
+            self.layers[key].W = self.params['W' + str(i+1)]
+            self.layers[key].b = self.params['b' + str(i+1)]
+        
+
+### 학습을 위한 코드
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+from common.trainer import Trainer
+# 데이터 읽기
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=False)
+
+# 시간이 오래 걸릴 경우 데이터를 줄인다.
+#x_train, t_train = x_train[:5000], t_train[:5000]
+#x_test, t_test = x_test[:1000], t_test[:1000]
+
+max_epochs = 10
+
+network = SimpleConvNet(input_dim=(1,28,28), 
+                        conv_param = {'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
+                        hidden_size=100, output_size=10, weight_init_std=0.01)
+                        
+trainer = Trainer(network, x_train, t_train, x_test, t_test,
+                  epochs=max_epochs, mini_batch_size=100,
+                  optimizer='Adam', optimizer_param={'lr': 0.001},
+                  evaluate_sample_num_per_epoch=1000)
+trainer.train()
+
+# 매개변수 보존
+network.save_params("params.pkl")
+print("Saved Network Parameters!")
+
+# 그래프 그리기
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(max_epochs)
+plt.plot(x, trainer.train_acc_list, marker='o', label='train', markevery=2)
+plt.plot(x, trainer.test_acc_list, marker='s', label='test', markevery=2)
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
+
+########################
+# 합성 곱 계층 시각화
+def filter_show(filters, nx=8, margin=3, scale=10):
+    """
+    c.f. https://gist.github.com/aidiary/07d530d5e08011832b12#file-draw_weight-py
+    """
+    FN, C, FH, FW = filters.shape
+    ny = int(np.ceil(FN / nx))
+
+    fig = plt.figure()
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+
+    for i in range(FN):
+        ax = fig.add_subplot(ny, nx, i+1, xticks=[], yticks=[])
+        ax.imshow(filters[i, 0], cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.show()
+
+
+network = SimpleConvNet()
+# 무작위(랜덤) 초기화 후의 가중치
+filter_show(network.params['W1'])
+
+# 학습된 가중치
+network.load_params("params.pkl")
+filter_show(network.params['W1'])
